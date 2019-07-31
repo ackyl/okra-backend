459ccef926253a83100a32512df3bf43ceaad887
atckyl <ackylmohamad@gmail.com>
2019-07-31 12:39:26 +0700

    small change to comment

--- src/routers/albumRouter.js
+++ src/routers/albumRouter.js
@@ -41,21 +41,7 @@ const upstore = multer(
     }
 )
 
-//Test Upload Biasa
-router.post('/album', (req, res) => {
-    const qry1 = `INSERT INTO album SET ?`
-    
-    conn.query(qry1, req.body, (err, result) => {
-        if(err){
-            return res.send(err)
-        }
-    })
-
-    res.send('Berhasil!')
-})
-
-
-//Upload MP3s
+//Upload Album
 router.post('/album/uploads', upstore.array('mu', 20), (req, res) => {
 
     const qry1 = `INSERT INTO album (album_name, album_artist, genre, release_year, picture, price, upload_date) VALUES (?)`
@@ -125,29 +111,28 @@ router.post('/album/uploads', upstore.array('mu', 20), (req, res) => {
     res.send('Success!')
 })
 
-//Upload MP3
-router.post('/album/upload', upstore.single('mu'), (req, res) => {
-    const qry1 = `UPDATE album SET picture = '${req.file.filename}'
-                    WHERE album_id = 1`
-
-    conn.query(qry1, (err, result) => {
-        if(err) return res.send(err)
-
-        mumeta.parseFile(`upload/music/${req.file.filename}`, {native: true})
-        .then(metadata=>{
-            console.log(util.inspect(metadata, { showHidden: false, depth: null }))
-        })
-
-        res.send({
-            message: 'Upload berhasil',
-            filename: req.file.filename
-        })
-    })
-})
-
 //Testing Ground
 mumeta.parseFile(`upload/testing/test.mp3`, {native: true})
     .then(metadata=>{
     })
+
+/*
+1. Create Album / Register New Album [DONE]
+2. Delete Album
+3. Create User / Register
+4. Login (User Type)
+5. Update User / Edit Profile
+6. Delete User
+7. Create Cart / Add to Cart
+8. Checkout / Update Cart
+9. View Albums
+10. View User Cart
+11. View User Transaction
+12. View User Transaction History
+13. View Tracks on Album
+14. View User / View Profile Detail
+15. View Users Transaction / Review Bukti Pembayran
+16. Update Users Transaction / Update to Accepted or Declined
+*/
 
 module.exports = router
